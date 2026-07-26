b5d0eb1fce33d823bb3fee464e78fe1f5b4cdab2
jljl1337 <lckjack123@gmail.com>
2026-07-26 17:34:13 -0600

    feat: implement core server module with configuration options and graceful shutdown

---
+++ pkg/core/server/option.go
@@ -0,0 +1,167 @@
+package server
+
+import (
+	"embed"
+	"io/fs"
+	"net/http"
+	"time"
+
+	"github.com/jljl1337/gostarter/pkg/core/cron"
+	"github.com/jljl1337/gostarter/pkg/core/http/common"
+	"github.com/jljl1337/gostarter/pkg/core/http/handler"
+	"github.com/jljl1337/gostarter/pkg/core/http/handler/endpoint"
+	"github.com/jljl1337/gostarter/pkg/core/http/handler/web"
+	"github.com/jljl1337/gostarter/pkg/core/http/middleware"
+	"github.com/jljl1337/gostarter/pkg/core/service"
+	serviceCron "github.com/jljl1337/gostarter/pkg/core/service/cron"
+	serviceEndpoint "github.com/jljl1337/gostarter/pkg/core/service/endpoint"
+	"github.com/jljl1337/gostarter/pkg/shared/crypto"
+	"github.com/jljl1337/gostarter/pkg/shared/validation"
+)
+
+type Option func(*Server) error
+
+func WithGostarterMigration() Option {
+	return func(s *Server) error {
+		s.runMigrations = true
+		s.runGostarterMigrations = true
+		return nil
+	}
+}
+
+func WithAppMigrations(appMigrationFS embed.FS) Option {
+	return func(s *Server) error {
+		s.runMigrations = true
+		s.appMigrationFS = appMigrationFS
+		return nil
+	}
+}
+
+func WithCustomIDGenerator(idGenerator func() string) Option {
+	return func(s *Server) error {
+		s.idGenerator = idGenerator
+		return nil
+	}
+}
+
+func WithCustomHashingManager(hashingManager *crypto.HashingManager) Option {
+	return func(s *Server) error {
+		s.hashingManager = hashingManager
+		return nil
+	}
+}
+
+func WithCustomValidationManager(validationManager *validation.ValidationManager) Option {
+	return func(s *Server) error {
+		s.validationManager = validationManager
+		return nil
+	}
+}
+
+func WithCustomResponseHandler(responseHandler *common.ResponseHandler) Option {
+	return func(s *Server) error {
+		s.responseHandler = responseHandler
+		return nil
+	}
+}
+
+func WithCustomCookieGenerator(cookieGenerator *common.CookieGenerator) Option {
+	return func(s *Server) error {
+		s.cookieGenerator = cookieGenerator
+		return nil
+	}
+}
+
+func WithDefaultScheduler(jobList ...cron.Job) Option {
+	return func(s *Server) error {
+		schedulerService := serviceCron.NewSchedulerService(s.db)
+		defaultJobList := cron.DefaultSchedulerJobFromEnv(schedulerService)
+
+		return WithScheduler(append(defaultJobList, jobList...)...)(s)
+	}
+}
+
+func WithScheduler(jobList ...cron.Job) Option {
+	return func(s *Server) error {
+		scheduler, err := cron.NewScheduler()
+		if err != nil {
+			return err
+		}
+
+		for _, j := range jobList {
+			if err := scheduler.AddJob(j); err != nil {
+				return err
+			}
+		}
+		s.scheduler = scheduler
+		return nil
+	}
+}
+
+func WithPort(port string) Option {
+	return func(s *Server) error {
+		s.port = port
+		return nil
+	}
+}
+
+func WithGracefulShutdownTimeout(timeout time.Duration) Option {
+	return func(s *Server) error {
+		s.gracefulShutdownTimeout = timeout
+		return nil
+	}
+}
+
+func WithStaticSite(path string, siteFs fs.FS) Option {
+	return func(s *Server) error {
+		webHandler := web.NewWebHandler(path, siteFs)
+		webHandler.RegisterRoutes(s.mux)
+
+		return nil
+	}
+}
+
+func WithDefaultMiddleware() Option {
+	return func(s *Server) error {
+		middlewareService := service.NewMiddlewareService(s.db)
+		middlewareProvider := middleware.NewMiddlewareProvider(middlewareService, s.responseHandler)
+		return WithMiddleware(middlewareProvider.GetMiddlewareList()...)(s)
+	}
+}
+
+func WithMiddleware(middlewareList ...middleware.Middleware) Option {
+	return func(s *Server) error {
+		s.apiMiddleware = middleware.CreateStack(middlewareList...)
+		return nil
+	}
+}
+
+func WithDefaultApiHandler(handlerList ...handler.Handler) Option {
+	return func(s *Server) error {
+		endpointService := serviceEndpoint.NewEndpointService(s.db, s.idGenerator, s.hashingManager, s.validationManager)
+		endpointHandler := endpoint.NewEndpointHandler(endpointService, s.responseHandler, s.cookieGenerator)
+		handlerList = append([]handler.Handler{endpointHandler}, handlerList...)
+		return WithApiHandler("/api", handlerList...)(s)
+	}
+}
+
+func WithApiHandler(subpath string, handlerList ...handler.Handler) Option {
+	return func(s *Server) error {
+		for _, h := range handlerList {
+			h.RegisterRoutes(s.apiMux)
+		}
+
+		s.mux.Handle(subpath+"/", http.StripPrefix(subpath, s.apiMiddleware(s.apiMux)))
+		return nil
+	}
+}
+
+func WithHttpServer() Option {
+	return func(s *Server) error {
+		s.httpServer = &http.Server{
+			Addr:    ":" + s.port,
+			Handler: s.mux,
+		}
+		return nil
+	}
+}
---
+++ pkg/core/server/server.go
@@ -0,0 +1,163 @@
+package server
+
+import (
+	"context"
+	"embed"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+
+	"github.com/jljl1337/gostarter/pkg/core/cron"
+	"github.com/jljl1337/gostarter/pkg/core/http/common"
+	"github.com/jljl1337/gostarter/pkg/core/http/middleware"
+	"github.com/jljl1337/gostarter/pkg/core/migration"
+	"github.com/jljl1337/gostarter/pkg/shared/crypto"
+	"github.com/jljl1337/gostarter/pkg/shared/env"
+	"github.com/jljl1337/gostarter/pkg/shared/generator"
+	"github.com/jljl1337/gostarter/pkg/shared/log"
+	"github.com/jljl1337/gostarter/pkg/shared/validation"
+)
+
+type Server struct {
+	db                      *sqlx.DB
+	runMigrations           bool
+	runGostarterMigrations  bool
+	appMigrationFS          embed.FS
+	idGenerator             func() string
+	hashingManager          *crypto.HashingManager
+	validationManager       *validation.ValidationManager
+	responseHandler         *common.ResponseHandler
+	cookieGenerator         *common.CookieGenerator
+	apiMux                  *http.ServeMux
+	mux                     *http.ServeMux
+	apiMiddleware           middleware.Middleware
+	port                    string
+	gracefulShutdownTimeout time.Duration
+	httpServer              *http.Server
+	scheduler               *cron.Scheduler
+}
+
+func NewServer(db *sqlx.DB, options ...Option) (*Server, error) {
+	hashingManager, err := crypto.NewHashingManagerFromEnv()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create hashing manager: %w", err)
+	}
+
+	validationManager, err := validation.NewDefaultValidationManager()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create validation manager: %w", err)
+	}
+	server := &Server{
+		db:                      db,
+		runMigrations:           false,
+		runGostarterMigrations:  false,
+		port:                    env.Port,
+		gracefulShutdownTimeout: time.Duration(env.GracefulShutdownTimeoutSec) * time.Second,
+		apiMux:                  http.NewServeMux(),
+		mux:                     http.NewServeMux(),
+		idGenerator:             generator.NewULID,
+		hashingManager:          hashingManager,
+		validationManager:       validationManager,
+		responseHandler:         common.NewDefaultResponseHandler(),
+		cookieGenerator:         common.NewCookieGeneratorFromEnv(),
+	}
+
+	for _, option := range options {
+		if err := option(server); err != nil {
+			return nil, fmt.Errorf("failed to apply server option: %w", err)
+		}
+	}
+
+	return server, nil
+}
+
+func (s *Server) StartWithGracefulShutdown() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	startErrCh := make(chan error, 1)
+	go func() {
+		startErrCh <- s.Start()
+	}()
+
+	select {
+	case err := <-startErrCh:
+		if err != nil {
+			log.Errorf("Server stopped with error: %v", err)
+		}
+	case <-ctx.Done():
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), s.gracefulShutdownTimeout)
+		defer cancel()
+
+		if err := s.Stop(shutdownCtx); err != nil {
+			log.Errorf("Failed to stop server gracefully: %v", err)
+		}
+
+		if err := <-startErrCh; err != nil {
+			log.Errorf("Server stopped with error: %v", err)
+		}
+	}
+}
+
+func (s *Server) Start() error {
+	log.Info("Starting server")
+
+	log.Info("Testing database connection")
+	if err := s.db.Ping(); err != nil {
+		return fmt.Errorf("failed to ping database: %w", err)
+	}
+	log.Info("Database connection successful")
+
+	if s.runMigrations {
+		log.Info("Running migrations")
+		migration.Migrate(s.db, s.runGostarterMigrations, s.appMigrationFS)
+	}
+
+	if s.scheduler != nil {
+		log.Info("Starting scheduler")
+		s.scheduler.Start()
+	}
+
+	if s.httpServer != nil {
+		log.Infof("Starting http server on %s", s.httpServer.Addr)
+		err := s.httpServer.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return fmt.Errorf("failed to start http server: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+	log.Info("Stopping server")
+
+	if s.httpServer != nil {
+		log.Info("Stopping HTTP server")
+		if err := s.httpServer.Shutdown(ctx); err != nil {
+			return fmt.Errorf("failed to stop HTTP server: %w", err)
+		}
+	}
+
+	if s.scheduler != nil {
+		log.Info("Stopping scheduler")
+		if err := s.scheduler.Shutdown(ctx); err != nil {
+			return fmt.Errorf("failed to stop scheduler: %w", err)
+		}
+	}
+
+	log.Info("Closing database connection")
+	if err := s.db.Close(); err != nil {
+		return fmt.Errorf("failed to close database connection: %w", err)
+	}
+
+	log.Info("Server stopped successfully")
+
+	return nil
+}
