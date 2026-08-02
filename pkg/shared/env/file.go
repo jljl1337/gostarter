6c487a6d36217440f3d5a42c719662fe3d58e0a2
jljl1337 <lckjack123@gmail.com>
2026-08-02 07:29:33 -0600

    refactor: update comments in LoadOptionalEnvFile for clarity

--- pkg/shared/env/file.go
+++ pkg/shared/env/file.go
@@ -19,6 +19,9 @@ func MustLoadOptionalEnvFile(files ...string) {
 // .env files. If a file does not exist, it will be ignored, allowing the
 // application to proceed with existing environment variables. If any other
 // error occurs while loading the .env files, it will return that error.
+//
+// If no files are provided, it will attempt to load a default .env file in the
+// current working directory.
 func LoadOptionalEnvFile(files ...string) error {
 	// It's okay if the .env file doesn't exist, we can proceed with existing env vars
 	if err := godotenv.Load(files...); err != nil && !errors.Is(err, os.ErrNotExist) {
