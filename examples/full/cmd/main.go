bce22fe734c4ef236b43012b309c8d715a2129c1
jljl1337 <lckjack123@gmail.com>
2026-08-02 07:15:27 -0600

    feat: add environment file support for full example server initialization

--- examples/full/cmd/main.go
+++ examples/full/cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/jljl1337/gostarter/examples/full/internal/server"
 )
 
 func main() {
-	server := server.MustNewServer()
+	envFile := flag.String("env", ".env", "Path to the .env file")
+	flag.Parse()
+
+	server := server.MustNewServer(*envFile)
 	server.StartWithGracefulShutdown()
 }
