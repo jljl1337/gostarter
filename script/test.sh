a698a3050207e0ca0ab86196817b0be22c4193be
jljl1337 <lckjack123@gmail.com>
2026-08-02 09:10:17 -0600

    test: add end-to-end tests setup with Docker Compose and application cleanup script

---
+++ script/test.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+app_pids=()
+start_pwd=$(pwd)
+
+cleanup() {
+    cd "$start_pwd"
+
+    echo "Cleaning up application processes..."
+	local exit_code=$?
+
+    echo "Shutting down the database compose..."
+	docker compose -f "test/full.compose.yml" down
+
+	set +e
+
+    if [ ${#app_pids[@]} -eq 0 ]; then
+        echo "No application processes to clean up."
+        exit "$exit_code"
+    fi
+
+    echo "Stopping the application processes..."
+	for pid in "${app_pids[@]}"; do
+        echo "Killing application process with PID $pid..."
+		kill "$pid" 2>/dev/null || true
+	done
+
+    echo "Waiting for application processes to exit..."
+	for pid in "${app_pids[@]}"; do
+        echo "Waiting for application process with PID $pid to exit..."
+		wait "$pid" 2>/dev/null || true
+	done
+
+    echo "Cleanup complete. Exiting with code $exit_code."
+	exit "$exit_code"
+}
+
+trap cleanup EXIT INT TERM
+
+echo "Building the binary..."
+go build -o full.out examples/full/cmd/main.go
+
+echo "Running the database compose..."
+docker compose -f test/full.compose.yml up -d
+
+echo "Running the application..."
+
+./full.out -env=test/sqlite.env > /dev/null 2>&1 &
+sqlite_pid=$!
+app_pids+=($sqlite_pid)
+echo "Started the application with SQLite PID: $sqlite_pid"
+
+./full.out -env=test/pg.env > /dev/null 2>&1 &
+pg_pid=$!
+app_pids+=($pg_pid)
+echo "Started the application with PostgreSQL PID: $pg_pid"
+
+./full.out -env=test/mysql.env > /dev/null 2>&1 &
+mysql_pid=$!
+app_pids+=($mysql_pid)
+echo "Started the application with MySQL PID: $mysql_pid"
+
+cd test
+echo "Running the tests..."
+set +e
+pnpm exec playwright test
+set -e
+cd ..
